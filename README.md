0ea7c445ee56fcd56024b29943e238be1659ec16
Stephen Akinrodoye <stephenakinrodoye2@gmail.com>
2024-03-30 18:11:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a personal portfolio website showcasing projects, resume, and contact information. 
+The website is responsiveness with incorporated CSS animations for an engaging user experience.
+
+Javascript is used to display different pages of the website as well as to achieve adequate user interaction.
---
+++ script.js
@@ -0,0 +1,31 @@
+const home = document.getElementById("home");
+const aboutMe = document.getElementById("about-me");
+const socialLinks = document.getElementById("social-links");
+const homeImgHover = document.getElementById("home-img-hover");
+const projectsContainer = document.getElementById("projects-container");
+
+function funcAbout() {
+    aboutMe.style.display = "block";
+    aboutMe.style.display = "flex";
+    home.style.display = "none";
+    socialLinks.style.display = "none";
+    homeImgHover.style.display = "none";
+}
+
+// document.addEventListener("click", funcAbout);
+
+function myHome() {
+    home.style.display = "block";
+    home.style.display = "flex";
+    aboutMe.style.display = "none";
+    socialLinks.style.display = "block";
+    homeImgHover.style.display = "block";
+
+}
+
+function projects() {
+    projectsContainer.style.display = "block";
+    home.style.display = "none";
+    aboutMe.style.display = "none";
+    homeImgHover.style.display = "none";
+}
